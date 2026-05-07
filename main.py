142341484e42e3ae1539ef10c774ee7b53271677
darknetbot4242-stack <darknetbot4242@gmail.com>
2026-05-07 20:57:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import runpy
 
-runpy.run_path("balina_avcisi_v5274_ict_pro_long_short_ayri_motor.py", run_name="main")
+runpy.run_path("main_v6_profesyonel_ai_birlesik_dataclass_fix.py", run_name="__main__")
